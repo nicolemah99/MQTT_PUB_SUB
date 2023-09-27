a6632166c0278d1f06695e37d9ab1ba4a7847317
nicolemah99 <nicolemah99@gmail.com>
2023-09-27 10:05:28 -0800

    re-organized functions

--- mqtt_broker_connection.py
+++ mqtt_broker_connection.py
@@ -10,14 +10,29 @@ client_id = f'subscribe-{random.randint(0, 100)}'
 # username = 'emqx'
 # password = 'public'
 
+# Callback function: triggered when broker responds to connection request
+def on_connect(client, userdata, flags, rc):
+    if rc == 0:
+        print("Successfully connected to MQTT Broker.\n")
+    else:
+        print("Failed to connect to MQTT Broker, return code %d\n", rc)
 
-def connect_mqtt() -> mqtt_client: # returns instance of mqtt_client
-    def on_connect(client, userdata, flags, rc):
-        if rc == 0:
-            print("Connected to MQTT Broker!")
-        else:
-            print("Failed to connect, return code %d\n", rc)
+# Callback function: triggered once client receives a message from the MQTT Broker
+def on_message(client, userdata, msg):
+    print(f"Received `{msg.payload.decode()}` from `{msg.topic}` topic")
 
+
+# Callback function: triggered once client receives a message from the MQTT Broker
+def on_subscribe(client, userdata, mid, granted_qos):
+    print(client)
+    print(userdata)
+    print(mid)
+    if granted_qos[0] == 0:
+        print(f"Successfully subscribed to topic! QoS: {granted_qos[0]}")
+    else:
+        print("Failed to subscribe!")
+
+def connect_mqtt() -> mqtt_client:  # returns instance of mqtt_client
     client = mqtt_client.Client(client_id)
     # client.username_pw_set(username, password)
     client.on_connect = on_connect
@@ -26,12 +41,9 @@ def connect_mqtt() -> mqtt_client: # returns instance of mqtt_client
 
 
 def subscribe(client: mqtt_client):
-    # Callback function: triggered once client receives a message from the MQTT Broker
-    def on_message(client, userdata, msg):
-        print(f"Received `{msg.payload.decode()}` from `{msg.topic}` topic")
-
-    client.subscribe(topic)
+    client.on_subscribe = on_subscribe
     client.on_message = on_message
+    client.subscribe(topic)
 
 
 def run():
